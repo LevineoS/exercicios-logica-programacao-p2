942d1c29d9f2c2b4a923ced805e1c20d2b733c57
LevineoS <levisantosbr5@gmail.com>
2024-11-19 21:25:12 -0300

    Update ex16.js

--- Lista 2 PA/Ex 16 np/JS/ex16.js
+++ Lista 2 PA/Ex 16 np/JS/ex16.js
@@ -5,6 +5,7 @@ var c = parseFloat(prompt("Insira o valor c: "));
 if(a == 0){;
     console.log("Se o a é igual a 0, não é uma equação de segundo grau.");
     alert("Se o a é igual a 0, não é uma equação de segundo grau.");
+
 }else{;
     var delta = (b*b) - (4 * a * c);
 
@@ -14,10 +15,12 @@ if(a == 0){;
 
     }else if(delta == 0){;
     var raiz = -b / (2 * a);
-    console.log("O delta é igual a zero, possui uma raiz.");
-    alert("O delta é igual a zero, possui uma raiz.");
+    console.log("O delta é igual a zero, possui uma raiz real. ");
+    alert("O delta é igual a zero, possui uma raiz real. ");
 
     }else{;
-        
+        var raiz = -b / (2 * a);
+        console.log("O delta é positivo, logo possuindo duas raizes reais. ");
+        alert("O delta é positivo, logo possuindo duas raizes reais. ");
     }
 }
